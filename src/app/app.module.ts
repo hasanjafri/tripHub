9bfe78d431fda62eada8882858b1bf22c3fb3283
Hasan Jafri <hasan.jafri7@gmail.com>
2019-09-07 22:00:18 -0400

    WIP: Preferences tab

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,13 +5,19 @@ import { Geolocation } from '@ionic-native/geolocation/ngx';
 import { SplashScreen } from '@ionic-native/splash-screen/ngx';
 import { StatusBar } from '@ionic-native/status-bar/ngx';
 import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
+import { IonicStorageModule } from '@ionic/storage';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
 @NgModule({
 	declarations: [AppComponent],
 	entryComponents: [],
-	imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
+	imports: [
+		BrowserModule,
+		IonicModule.forRoot(),
+		AppRoutingModule,
+		IonicStorageModule.forRoot({ name: 'user-preferences', driverOrder: ['localstorage', 'sqlite', 'indexeddb', 'websql'] })
+	],
 	providers: [StatusBar, SplashScreen, Geolocation, { provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
 	bootstrap: [AppComponent]
 })
--- src/app/tab3/tab3.page.ts
+++ src/app/tab3/tab3.page.ts
@@ -1,10 +1,50 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Storage } from '@ionic/storage';
 
 @Component({
 	selector: 'app-tab3',
 	templateUrl: 'tab3.page.html',
 	styleUrls: ['tab3.page.scss']
 })
-export class Tab3Page {
-	constructor() {}
+export class Tab3Page implements OnInit {
+	preferencesList = [
+		{
+			title: 'Digital Entertainment',
+			subtitle: 'Video Games, VR Arcades and more!',
+			imgUrl: '../../assets/avatar-vg.jpg',
+			checked: false
+		},
+		{
+			title: 'Gambling',
+			subtitle: 'Casinos and other gambling sites!',
+			imgUrl: '../../assets/avatar-casino.jpg',
+			checked: false
+		},
+		{
+			title: 'Nature & Outdoor Sites',
+			subtitle: 'Parks and Recreational Sites',
+			imgUrl: '../../assets/avatar-park.jpg',
+			checked: false
+		}
+	];
+
+	constructor(private storage: Storage) {}
+
+	async ngOnInit() {
+		for (let i: number; i < this.preferencesList.length; i++) {
+			const existingPrefValue = await this.storage.get(this.preferencesList[i].title);
+			if (existingPrefValue) {
+				this.preferencesList[i].checked = existingPrefValue;
+			} else if (existingPrefValue !== false) {
+				this.storage.set(this.preferencesList[i].title, this.preferencesList[i].checked);
+			}
+		}
+	}
+
+	onCheckboxClick(checkbox, index) {
+		this.storage.set(this.preferencesList[index].title, checkbox.detail.checked);
+		this.storage.get(this.preferencesList[index].title).then((x) => {
+			console.log(x);
+		});
+	}
 }
